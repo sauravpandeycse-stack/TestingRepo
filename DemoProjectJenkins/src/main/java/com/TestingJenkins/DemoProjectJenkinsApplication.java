b39eacbb394f7d14381b4bd29e097e585521c63a
sauravpandey <saurav.pandey@nagarro.com>
2022-05-14 14:56:40 +0530

    Inital commit for Java jenkins code

---
+++ DemoProjectJenkins/src/main/java/com/TestingJenkins/DemoProjectJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.TestingJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectJenkinsApplication.class, args);
+	}
+
+}
